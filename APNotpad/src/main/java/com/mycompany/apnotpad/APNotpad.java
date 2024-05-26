67b08723b3a84fa1872b625c07d789814c7fd42e
MichaelWorkineh_git <122392569+MichaelWorkineh@users.noreply.github.com>
2024-05-26 20:23:11 +0200

    initiation!

---
+++ APNotpad/src/main/java/com/mycompany/apnotpad/APNotpad.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.apnotpad;
+
+/**
+ *
+ * @author worki
+ */
+public class APNotpad {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
